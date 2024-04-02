754d26883852db7a55c7b81c66d5c03314b11fa1
Treata11 <treata11@yahoo.com>
2024-04-02 17:25:39 +0330

    Added `View`s for the artistic shaders of `iShader` library.

---
+++ Sources/ColorEffect/Gradient/Gradients.swift
@@ -0,0 +1,17 @@
+/*
+ Gradients.swift
+ iShader
+
+ Created by Treata Norouzi on 2/29/24.
+ 
+ Abstract:
+ 
+*/
+
+import SwiftUI
+
+extension View {
+    // MARK: - Rotating Gradient
+    
+    
+}
