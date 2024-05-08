4f714d99567ebbefcc06ab299795760134c6e317
Treata11 <treata11@yahoo.com>
2024-05-08 17:56:41 +0330

    Successfully accessed binaries of metal-shader-functions in products directory; All Metal files are pre-compiled and merged into a per-module library named default.metallib.

--- Tests/iShaderTests/iShaderTests.swift
+++ Tests/iShaderTests/iShaderTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import Metal
 @testable import iShader
 @testable import ColorEffect
 
@@ -10,36 +11,24 @@ final class iShaderTests: XCTestCase {
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
     }
-    
-    /// Always fails ...
+
+    /// Always succeeds!
+    ///
+    /// Discussed at: https://forums.swift.org/t/difficulty-accessing-resources-in-a-packages-bundle/71598/5
+    /// Solved by [Christophe Bronner](https://github.com/Lancelotbronner)
     func testBundleSourceString() throws {
-        let fileName = "Shaders/CRT"
-        let bundle = Bundle.colorEffect
-        bundle.load()
-        
-        if bundle.isLoaded {
-            if let sourceURL = bundle.url(forResource: fileName, withExtension: "metal") {
-                let sourceString = try? String(contentsOf: sourceURL, encoding: .utf8)
-                print("Source String: \n\(sourceString!)")
-            } else {
-                XCTFail("testBundleSourceString FAILED ❌")
-            }
-        } else {
-            XCTFail("Failed to load ColorEffect module ❌")
+        let url = Bundle.colorEffect.url(forResource: "default", withExtension: "metallib")
+        XCTAssertNotNil(url, "Failed to load ColorEffect metal library")
+        if let url {
+            print(url)
         }
     }
-
-    func testColorEffectBundle() throws {
-        Bundle.colorEffect.load()
-        
-        print("""
-            Color-Effect-Module bundleIdentifier:
-                \(Bundle.colorEffect.bundleIdentifier ?? "NA")
-            Color-Effect-Module bundleURL:
-                \(Bundle.colorEffect.bundleURL)
-            Color-Effect-Module bundlePath:
-                \(Bundle.colorEffect.bundlePath)
-            
-            """)
+    
+    /// WIP
+    func testMakeDefaultLibrary() throws {
+        // Create a Metal device
+        let device = MTLCreateSystemDefaultDevice()!
+        let colorEffectLibrary = try device.makeDefaultLibrary(bundle: Bundle.colorEffect)
+//        let functionNames = colorEffectLibrary.functionNames
     }
 }
